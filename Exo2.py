b06e5852eccf01292aafd928d38cd116c67c1c48
yah samake <yanki.samake@gmail.com>
2022-10-17 23:24:18 +0000

    Exo2.py

---
+++ Exo2.py
@@ -0,0 +1,9 @@
+class Pet:
+    def _init_(self,name,animalType,age):
+        self.__name=name
+        self.__animalType=animalType
+        self.__age=age
+
+    input("veillez saisir un nom  ")
+    input("veillez saisir animalType  ")
+    input("veiller saisir l'age de l'animal  ")
